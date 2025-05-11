11c9ba8a2ff40848f33ccee2405ab1c1c25697c1
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-05-11 12:57:01 +0530

    done with the reading and writing functionalities

--- fileManager/fileManager.js
+++ fileManager/fileManager.js
@@ -1,14 +1,38 @@
 const fs = require("fs");
 const filePath = "./tasks.json";
 
+const loadTasks=()=>{
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+}
+
+const saveTasks=(tasks)=>{
+  const dataJSON = JSON.stringify(tasks)
+  fs.writeFileSync(filePath,dataJSON)
+}
+
+const addTask = (task)=>{
+  const tasks = loadTasks()
+  tasks.push(task)
+  saveTasks(tasks)
+  console.log("Task Added!", task)
+}
+
+
+
 const command = process.argv[2]
 const argument = process.argv[3]
 
-if(command===add){
+if(command==="add"){
   addTask(argument);
-}else if(command===list){
+}else if(command==="list"){
   listTask();
-}else if(command===remove){
+}else if(command==="remove"){
   removeTask(parseInt(argument));
 }else{
   console.log("Command not found!");
