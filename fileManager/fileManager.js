ddc76287026775cfa58929285472983fe2efa4cf
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-05-11 11:23:42 +0530

    setting up the logic

---
+++ fileManager/fileManager.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = "./tasks.json";
+
+
+if(command===add){
+  addTask(argument);
+}else if(command===list){
+  listTask();
+}else if(command===remove){
+  removeTask(parseInt(argument));
+}else{
+  console.log("Command not found!");
+}
