9af8b48ceb504c85a1382a57b025b13e4221edf5
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-05-12 16:15:15 +0530

    done with the server.js

--- NginxServer/server.js
+++ NginxServer/server.js
@@ -4,7 +4,30 @@ const path = require("path")
 
 const port = 3000
 
-const server = http.createServer()
+const server = http.createServer((req,res)=>{
+  const filePath = path.join(__dirname, req.url==="/"? "index.html": req.url)
+
+  const extName = String(path.extname(filePath)).toLowerCase()
+
+  const mimeTypes = {
+    ".html" : "text/html",
+    ".css" : "text/css",
+    ".js" : "text/javascript",
+    ".png" : "text/png",
+  }
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+
+  fs.readFile(filePath,(err,content)=>{
+    if(err){
+      res.writeHead(404,{"Content-Type": "text/html"})
+      res.end("404 File Not Found!! BRooooooo");
+    }else{
+      res.writeHead(200,{"Content-Type": "text/html"});
+      res.end(content,"utf-8")
+    }
+
+  })
+})
 server.listen(port,()=>{
   console.log(`Port is Listening ${port}`)
 })
